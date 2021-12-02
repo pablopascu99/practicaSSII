8e16217d7f5e1ce212761dc88cb8dc20f4454c91
pablopascu99 <pablopascu99@gmail.com>
2021-12-02 20:13:24 +0100

    El pais sanidad (falta fecha)

--- practica.py
+++ practica.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas
+import pandas as pd
 # URL de la web
 url = 'https://elpais.com/noticias/sanidad/'
 # Claves del navegador web
@@ -9,20 +9,39 @@ page = requests.get(url, headers = headers)
 # Si el status_code nos da un 200 es que es el request se ha hecho bien y si nos da un 400 es que no existe
 print(page.status_code)
 soup = BeautifulSoup(page.text, 'html.parser')
-print(soup.title.text)
 noticias = soup.find_all('h2', {'class': 'c_t' })
+allNoticas = []
 pages = []
 for url in noticias:
-    noticia = "https://elpais.com"+url.find('a')['href']
-    pages.append(noticia)
-print(pages)
-for page in pages:
-    noticia = requests.get(page, headers = headers)
+    url = {
+        'noticia' : "https://elpais.com"+url.find('a')['href']
+    }
+    pages.append(url)
+
+df = pd.DataFrame(pages)
+
+for page in range(len(df.index)):
+    noticia = requests.get(df.at[page,'noticia'], headers = headers)
     soupNoticia = BeautifulSoup(noticia.text, 'html.parser')
-    print(soupNoticia.title.text)
-    '''noticias = soup.find_all('h2', {'class': 'c_t' })
-    pages = []
-    for url in noticias:
-        noticia = "https://elpais.com"+url.find('a')['href']
-        pages.append(noticia)
-    print(pages)'''
+    contenido = soupNoticia.find_all('article', {'class': 'a _g _g-lg _g-o' })
+    for i in contenido:
+        estructuraTexto = soupNoticia.find('div', {'class': 'clearfix' })
+        childrenTexto = estructuraTexto.findChildren("p" , recursive=False)
+        texto=""
+        for childTexto in childrenTexto:
+            texto = texto + childTexto.text
+        estructuraEtiquetas = soupNoticia.find('ul', {'class': '_df _ls' })
+        childrenEtiquetas = estructuraEtiquetas.findChildren("li" , recursive=False)
+        tags=""
+        for childEtiquetas in childrenEtiquetas:
+            tags = tags + childEtiquetas.text + "|"
+        componentes = {
+            'titulo' : i.find('h1', {'class': 'a_t'}).text,
+            'entradilla' : i.find('h2', {'class':'a_st'}).text,
+            'texto' : texto,
+            'etiquetas' : tags,
+            'fecha' : i.find('a', {'id':'article_date_p'})
+        }
+        allNoticas.append(componentes)
+dfNoticias = pd.DataFrame(allNoticas)
+print(dfNoticias.at[2,'etiquetas'])
