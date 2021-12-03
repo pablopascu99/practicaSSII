65afb8ec00f8ef8070ad5db473eeccb66045103a
pablopascu99 <pablopascu99@gmail.com>
2021-12-03 18:58:30 +0100

    Web scraping de ElPais salud terminado

--- practica.py
+++ practica.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+import re
+
 # URL de la web
 url = 'https://elpais.com/noticias/sanidad/'
 # Claves del navegador web
@@ -9,44 +10,64 @@ page = requests.get(url, headers = headers)
 # Si el status_code nos da un 200 es que es el request se ha hecho bien y si nos da un 400 es que no existe
 print(page.status_code)
 soup = BeautifulSoup(page.text, 'html.parser')
+# Cogemos todas las etiquetas con clase c_t, que son las que nos interesan
+# porque tienen las url de cada noticia
 noticias = soup.find_all('h2', {'class': 'c_t' })
+# Guardamos todas las noticias con los campos: titulo, entradilla, 
+# fecha, texto y etiquetas
 allNoticas = []
+# Guardamos las url de las noticias
 pages = []
 for url in noticias:
-    url = {
-        'noticia' : "https://elpais.com"+url.find('a')['href']
-    }
-    pages.append(url)
-
-df = pd.DataFrame(pages)
-
-for page in range(len(df.index)):
-    noticia = requests.get(df.at[page,'noticia'], headers = headers)
+    noticia= "https://elpais.com"+url.find('a')['href']
+    
+    pages.append(noticia)
+# Contador para identificar el numero de archivo creado
+cont=1
+for page in range(len(pages)):
+    # Cogemos el cada noticia mediante su url
+    noticia = requests.get(pages[page], headers = headers)
     soupNoticia = BeautifulSoup(noticia.text, 'html.parser')
+    # Cogemos todas las etiquetas con clase a _g _g-lg _g-o, que son las que nos interesan
+    # porque tienen los campos que queremos de cada noticia
     contenido = soupNoticia.find_all('article', {'class': 'a _g _g-lg _g-o' })
-    for i in contenido:
-        estructuraTexto = soupNoticia.find('div', {'class': 'clearfix' })
-        childrenTexto = estructuraTexto.findChildren("p" , recursive=False)
-        texto=""
-        for childTexto in childrenTexto:
-            texto = texto + childTexto.text
-        estructuraEtiquetas = soupNoticia.find('ul', {'class': '_df _ls' })
-        childrenEtiquetas = estructuraEtiquetas.findChildren("li" , recursive=False)
-        tags=""
-        for childEtiquetas in childrenEtiquetas:
-            tags = tags + childEtiquetas.text + "|"
-        #La fecha esta en formato ISO
-        if i.find("a", {"id": "article_date_p"}) is not None:
-            fecha = i.find('a', id="article_date_p").attrs['data-date']
-        if i.find("a", {"id": "article_date_u"}) is not None:
-            fecha = i.find('a', id="article_date_u").attrs['data-date']
-        componentes = {
-            'titulo' : i.find('h1', {'class': 'a_t'}).text,
-            'entradilla' : i.find('h2', {'class':'a_st'}).text,
-            'texto' : texto,
-            'etiquetas' : tags,
-            'fecha' : fecha,
-        }
-        allNoticas.append(componentes)
-dfNoticias = pd.DataFrame(allNoticas)
-print(dfNoticias)
+    noticiaCampos=[]
+    # Cogemos la estructura que abarca el campo de texto
+    estructuraTexto = soupNoticia.find('div', {'class': 'clearfix' })
+    # Cogemos los parrafos hijos de la estructura de texto que conformaran
+    # todo el texto que necesitamos
+    childrenTexto = estructuraTexto.findChildren("p" , recursive=False)
+    texto=""
+    for childTexto in childrenTexto:
+        texto = texto + childTexto.text
+    # Cogemos las etiquetas hijas de la estructura de etiquetas que agruparan
+    # todas las etiquetas que necesitamos
+    estructuraEtiquetas = soupNoticia.find('ul', {'class': '_df _ls' })
+    childrenEtiquetas = estructuraEtiquetas.findChildren("li" , recursive=False)
+    tags=""
+    for childEtiquetas in childrenEtiquetas:
+        tags = tags + childEtiquetas.text + "|"
+    # Hay dos estructuras posibles en las fechas por lo que tenemos que 
+    # tener en cuenta ambos casos
+    if soupNoticia.find("a", {"id": "article_date_p"}) is not None:
+        # Guardamos la fecha
+        fecha = soupNoticia.find('a', id="article_date_p").attrs['data-date']
+        # Eliminamos la parte de la hora
+        delTime = re.sub('T.*', "", fecha)
+    if soupNoticia.find("a", {"id": "article_date_u"}) is not None:
+        # Guardamos la fecha
+        fecha = soupNoticia.find('a', id="article_date_u").attrs['data-date']
+        # Eliminamos la parte de la hora
+        delTime = re.sub('T.*', "", fecha)
+    # Guardamos el titulo
+    titulo = soupNoticia.find('h1', {'class': 'a_t'}).text
+    # Guardamos la entradilla
+    entradilla = soupNoticia.find('h2', {'class':'a_st'}).text
+
+    # Todo el string que meteremos al archivo de texto
+    noticiaElPais = titulo + "\n######\n" + entradilla + "\n######\n" + texto + "\n######\n" + tags + "\n######\n" + delTime
+    # Situamos y creamos los archivos de texto con los campos extraidos
+    file = open("./ElPais/" + "Salud/"+ "ElPais_Salud_"+ delTime + "_" + str(cont) + ".txt", "w", encoding="utf-8")
+    file.write(noticiaElPais)
+    file.close()
+    cont=cont+1
