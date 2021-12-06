768ac228971254a77141168b745e20d57cce69a5
pablopascu99 <pablopascu99@gmail.com>
2021-12-06 12:20:38 +0100

    borrar espacio

--- busqueda.py
+++ busqueda.py
@@ -8,14 +8,18 @@ fileA.close()
 fileB.close()
 # Separamos el texto de los archivos por palabras
 bagOfWordsA = re.split('\n######\n|\|| |\(|\)|\.|\,|\”|\“|\[|\]',docA) 
-bagOfWordsB = re.split('\n######\n|\|| |\(|\)|\.|\,|\”|\“|\[|\]',docB) 
+bagOfWordsB = re.split('\n######\n|\|| |\(|\)|\.|\,|\”|\“|\[|\]',docB)
 # Pasamos las palabras a un mismo conjunto para evitar duplicidades
 uniqueWords = set(bagOfWordsA).union(set(bagOfWordsB))
 # Creamos un diccionario y contamos las ocurrencias de palabras en cada archivo
 numOfWordsA = dict.fromkeys(uniqueWords, 0)
 for word in bagOfWordsA:
     numOfWordsA[word] += 1
+print(len(numOfWordsA.keys()))
+del numOfWordsA['']
+print(len(numOfWordsA.keys()))
 numOfWordsB = dict.fromkeys(uniqueWords, 0)
 for word in bagOfWordsB:
     numOfWordsB[word] += 1
-print(numOfWordsA)
+del numOfWordsB['']
+#print(numOfWordsA)
